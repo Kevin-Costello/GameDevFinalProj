f19399a96333f1d2387ce97aaced299adf5098ac
Kevin-Costello <kevin.costello1@marist.edu>
2020-11-21 10:46:25 -0500

    Added Character Controller
    The player can walk around used WASD, crouch with Left Controll and wall run. Probably needs a little tweeking but I declared public variables for most of the physics based mechanics such as the wall friction and  wall "stickage"

---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Apply this script to the parent camera object
+//Using the parent object helps prevent some weird camera movement and jittering
+public class MoveCamera : MonoBehaviour
+{
+    public Transform playerHead;
+
+    void Update()
+    {
+        transform.position = playerHead.transform.position;
+    }
+}
